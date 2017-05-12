431e56f07cba03753a85b468caf0d62eac957552
asitaru <andreisitaru93@gmail.com>
2017-05-12 18:33:59 +0300

    Customer View back-end and front-end logic finished

--- public/js/geolocation.js
+++ public/js/geolocation.js
@@ -1,21 +1,24 @@
 document.getElementById("currentLocation").addEventListener('click', getGeolocation);
 document.getElementById("findLocation").addEventListener('click', findAddress);
 
+//functie pentru detectarea pozitiei prin gps
 function getGeolocation(){
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
-            console.log({
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            });
 
-            var xhr = new XMLHttpRequest();
-            xhr.open("POST", 'http://localhost:5000/location', true);
-            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-            xhr.send(latitudine = position.coords.latitude);
+
+            //dupa ce coordonatele sunt gasite, se trimit la server
+            var form = document.createElement('form');
+            form.setAttribute('method', 'post');
+            var url = 'http://localhost:5000/location?latitudine=' + position.coords.latitude  + '&longitudine=' + position.coords.longitude;
+            form.setAttribute('action', url);
+            form.style.display = 'hidden';
+            document.body.appendChild(form);
+            form.submit();
+
 
         }, function() {
-           console.log('Error!');
+           alert('Ceva nu a mers cum trebuie, va rugam reincercati');
         });
     } else {
         // Browser doesn't support Geolocation
@@ -23,6 +26,7 @@ function getGeolocation(){
     }
 }
 
+//functie pentru detectarea geolocatiei prin search
 function findAddress(){
     var address = document.getElementById("address").value.split(' ').join('+');
     var url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=AIzaSyBMN28F6Ef-4HFF_E4yzZDHXfBUDDGehCQ";
@@ -42,7 +46,19 @@ function findAddress(){
 function alertContents() {
     if (httpRequest.readyState === XMLHttpRequest.DONE) {
         if (httpRequest.status === 200) {
-            console.log(JSON.parse(httpRequest.responseText).results[0].geometry.location);
+
+            //aduna informatia
+            var latitude = JSON.parse(httpRequest.responseText).results[0].geometry.location.lat;
+            var longitude = JSON.parse(httpRequest.responseText).results[0].geometry.location.lng;
+
+            //trimite informatia la server prin POST
+            var form = document.createElement('form');
+            form.setAttribute('method', 'post');
+            var url = 'http://localhost:5000/location?latitudine=' + latitude  + '&longitudine=' + longitude;
+            form.setAttribute('action', url);
+            form.style.display = 'hidden';
+            document.body.appendChild(form);
+            form.submit();
         } else {
             alert('Va rugam sa alegeti o adresa valida');
         }
--- server/controllers/MapController.js
+++ server/controllers/MapController.js
@@ -3,28 +3,31 @@ const distance = require('google-distance');
 
 module.exports = (req, res) => {
 
-    console.log(req.body.latitudine);
+    console.log(req);
     Contact.getAllContacts(function(err,contacts){
         if(err) throw err;
+
         contacts.map(contact => {
-            let distanta = Math.sqrt((req.body.latitudine - contact.latitudine)**2 * 10**8 + (req.body.longitudine - contact.longitudine)**2 * 10**8)/ 10000;
+            let distanta = Math.sqrt((req.query.latitudine - contact.latitudine)**2 * 10**8 + (req.query.longitudine - contact.longitudine)**2 * 10**8)/ 10000;
             contact.distanta = distanta.toFixed(2);
         });
+
         contacts.sort((a,b) => a.distanta - b.distanta);
-        //contacts.forEach(contact => console.log(contact.distanta));
+
         if(contacts.length > 10){
-            contacts = contacts.slice
+            contacts = contacts.slice(0,10);
         }
+
         let adrese = contacts.map(contact => contact.latitudine + ',' + contact.longitudine);
-        //console.log(adrese);
 
         distance.get(
             {
-                origin: req.body.latitudine + ',' + req.body.longitudine,
+                origin: req.query.latitudine + ',' + req.query.longitudine,
                 destinations: adrese
             },
             function(err, data) {
                 if (err) return console.log(err);
+                console.log(data);
                 contacts.forEach(function(contact, index, contacts){
                     contact.distanta = data[index].distance;
                     contact.durata = data[index].durationValue;
